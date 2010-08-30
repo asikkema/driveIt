874c501d6b6b375fb488087b7e2e54198c011adc
Albert Sikkema <a.sikkema80@gmail.com>
2010-08-30 16:55:36 +0200

    now using contacts provider

--- driveIt/src/nl/sikkema/driveit/NewStudent.java
+++ driveIt/src/nl/sikkema/driveit/NewStudent.java
@@ -1,7 +1,11 @@
 package nl.sikkema.driveit;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class NewStudent extends Activity {
 
@@ -9,5 +13,12 @@ public class NewStudent extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.new_student);
+		Button btn = (Button) findViewById(R.id.SaveNewStudentButton);
+		btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+			}
+		});
 	}
 }
--- driveIt/src/nl/sikkema/driveit/StudentSelect.java
+++ driveIt/src/nl/sikkema/driveit/StudentSelect.java
@@ -1,8 +1,14 @@
 package nl.sikkema.driveit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract.Contacts;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
@@ -14,16 +20,29 @@ import android.widget.AdapterView.OnItemClickListener;
 
 public class StudentSelect extends Activity {
 
-	private static String[] students = {"Jan", "Paul", "Hendrik", "Peter", "Mike", "Anita", "Berber", "Chantal", "Rutger", "Auke", "Diana", "Henk"};
+//	private static String[] students = {"Jan", "Paul", "Hendrik", "Peter", "Mike", "Anita", "Berber", "Chantal", "Rutger", "Auke", "Diana", "Henk"};
 	private ListView studentsList;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.student_select);
+		
+		String[] projection = new String[] {Contacts.DISPLAY_NAME};
+		Uri contacts = Contacts.CONTENT_URI;
+		
+		List<String> students = new ArrayList<String>(); 
+		Cursor cur = managedQuery(contacts, projection, null, null, null);
+		if(cur.moveToFirst()) {
+			int displayNameIndex = cur.getColumnIndex(Contacts.DISPLAY_NAME);
+			String name = cur.getString(displayNameIndex);
+			students.add(name);
+		}
 		studentsList = (ListView) findViewById(R.id.studentsList);
 		studentsList.setTextFilterEnabled(true);
 		studentsList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, students));
+		
+		
 		studentsList.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
